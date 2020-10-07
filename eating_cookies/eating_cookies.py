3f86865711eb566f33091c71a2d09b1783a531e7
jasonbringle <jasonbringle@gmail.com>
2020-10-06 21:49:59 -0700

    finished MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,22 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    if n == 0:
+        return 1
 
-    pass
+    if n == 1:
+        return 1
+
+    if n == 2:
+        return 2
+    
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    
+    
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 4
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,27 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # # Your code here
+    # #Array to put numbers in
+    # arrK = []
+    # maxNums = []
+    # i = 0
+    # j = 0
+    # #For each number in range of the array - k +1 (this prevents looping with non-existant indexes)
+    # for i in range(len(nums)-k+1):#O(N)
+    #     #Use an iterator in range of 
+    #     while j in range(k):#O(k*N)
+    #         arrK.append(nums[i+j])#O(C)
+    #         j += 1#O(C)
+    #         if len(arrK) == k:#O(C)
+    #             maxNums.append(max(arrK)) #O(C) 
+    #             arrK = []#O(C)
+    #             j = 0 #O(C)
+    #             break
+        
+    maxNums = [max(nums[i:i+k]) for i in range(len(nums)-k+1)]
 
-    pass
+    return maxNums
 
 
 if __name__ == '__main__':

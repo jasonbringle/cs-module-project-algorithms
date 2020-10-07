c0d0e9c207e402d0582ae6c708a7530624150078
jasonbringle <jasonbringle@gmail.com>
2020-10-06 23:06:10 -0700

    Updated code with remarks

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,13 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
+    #Checking for base case at the outset. This says there is one possible way to eat 0 cookies. 0
     if n == 0:
         return 1
-
+    #This says there is one way to eat one cookie
     if n == 1:
         return 1
-
+    #This says there are 2 ways to eat 2 cookies
     if n == 2:
         return 2
     
@@ -19,6 +20,6 @@ def eating_cookies(n):
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 4
+    num_cookies = 3
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")

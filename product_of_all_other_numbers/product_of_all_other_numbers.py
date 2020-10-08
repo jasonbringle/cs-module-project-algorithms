2aae4cd799cd9ab9737d248a66d90869604364e5
jasonbringle <jasonbringle@gmail.com>
2020-10-07 18:29:55 -0700

    added comments

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,25 +3,41 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
+    #This houses the total of all items minus the integer we are currently on
     total = 0
+    #This houses each individual total for all the numbers
     final = []
     i = 0
     j = 0
+    #Loop through all the numbers
     for i in range(len(arr)):
+        #Reset j to 0 so when while loop starts it starts at zero
         j = 0
+        #Append any values in total to the final array
         final.append(total)
+        #Reset total to 0 so the next set of operations adds to a clean variable
         total = 0
+        #For each item while look at the number
         while j in range(len(arr)):
+            # and determine if it is greater than 1
             if total >= 1:
+                #If the number doesnt equal the for loop iterator
                 if j != i :
+                    #Add the number in the while loop to the total
                     total *= arr[j]
+                    #Increment j to move to the next number
                     j += 1
                 else:
+                    #Skip the operation if the j = i.  This is how we prevent from including the current number in our operation.
                     j += 1
+            #If total is 0 then it is at the start of the operation and needs to add a 1 to begin the operation.
             else:
                 total += 1
+    #Append the final number
     final.append(total)
+    #Pop off the zero at the start which is created on first run on line 17
     final.pop(0)
+    #return array
     return final
     
 
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,25 +3,33 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # # Your code here
-    # #Array to put numbers in
-    # arrK = []
-    # maxNums = []
-    # i = 0
-    # j = 0
-    # #For each number in range of the array - k +1 (this prevents looping with non-existant indexes)
-    # for i in range(len(nums)-k+1):#O(N)
-    #     #Use an iterator in range of 
-    #     while j in range(k):#O(k*N)
-    #         arrK.append(nums[i+j])#O(C)
-    #         j += 1#O(C)
-    #         if len(arrK) == k:#O(C)
-    #             maxNums.append(max(arrK)) #O(C) 
-    #             arrK = []#O(C)
-    #             j = 0 #O(C)
-    #             break
-        
-    maxNums = [max(nums[i:i+k]) for i in range(len(nums)-k+1)]
+    # Your code here
+    #Array to put numbers in
+    arrK = []
+    maxNums = []
+    i = 0
+    j = 0
+    #For each number in range of the array - k +1 (this prevents looping with non-existant indexes)
+    for i in range(len(nums)-k+1):#O(N)
+        #Use an iterator in range of k
+        while j in range(k):#O(k*N)
+            #Append the number to the array by using the for and while loop iterators.
+            arrK.append(nums[i+j])#O(C)
+            #increment the j variable to move to the next number in the array
+            j += 1#O(C)
+            #Check if the length of the array is as big as the window (k)
+            if len(arrK) == k:#O(C)
+                #apped the maximum number in the temporary array to the maxNums array
+                maxNums.append(max(arrK)) #O(C) 
+                #reset the temporary array
+                arrK = []#O(C)
+                #reset j to zero so when it starts incrementing again it will use i in the for loop as a reference
+                j = 0 #O(C)
+                #Break out from while loop to return to for loop and get the next set of k numbers
+                break
+    
+        #Not my solution
+    # maxNums = [max(nums[i:i+k]) for i in range(len(nums)-k+1)]
 
     return maxNums
 
